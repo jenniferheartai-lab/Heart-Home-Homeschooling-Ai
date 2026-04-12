32244590e6f2b1fb5c710a9849922309251f4b48
jenniferheartai-lab <jenniferheartai@gmail.com>
2026-04-12 11:03:18 -0400

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,1 +1,16 @@
 
+document.getElementById("nav").innerHTML = `
+<nav>
+<a href="../index.html">Home</a>
+<a href="reading.html">Reading</a>
+<a href="math.html">Math</a>
+<a href="writing.html">Writing</a>
+<a href="tutor-hub.html">Tutor Hub</a>
+</nav>
+`;
+
+document.getElementById("footer").innerHTML = `
+<footer>
+<p>1854 Digital Academy ©</p>
+</footer>
+`;
