411d7f61cce9060f59d042ea5cfa98f8a889c4d0
jenniferheartai-lab <jenniferheartai@gmail.com>
2026-04-12 11:14:29 -0400

    Update tutor.js

--- assets/js/tutor.js
+++ assets/js/tutor.js
@@ -1,1 +1,17 @@
 
+function send() {
+let input = document.getElementById("input").value;
+let chat = document.getElementById("chatbox");
+
+let response = "Capital! Let us explore this further.";
+
+if (input.includes("don't understand")) {
+response = "Pray allow me to explain it differently, dear scholar.";
+localStorage.setItem("confusion", input);
+}
+
+chat.innerHTML += "<p><b>You:</b> " + input + "</p>";
+chat.innerHTML += "<p><b>Mr. Hawthorne:</b> " + response + "</p>";
+
+localStorage.setItem("chat", chat.innerHTML);
+}
